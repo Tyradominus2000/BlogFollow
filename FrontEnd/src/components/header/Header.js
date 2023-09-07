1519db42050ad317db85e8370254c651553d1b8c
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-09-07 16:28:15 +0200

    feat : header have button

--- FrontEnd/src/components/header/Header.js
+++ FrontEnd/src/components/header/Header.js
@@ -1,7 +1,14 @@
+import "./Header.scss";
+import { NavLink } from "react-router-dom";
+import { Logout } from "../../apis/users/logout";
 export default function Header() {
   return (
-    <div>
-      <h2>Header</h2>
+    <div className="d-flex justify-content-around container">
+      <p>MonBlog</p>
+      <NavLink to={"/login"}>
+        <p>Login</p>
+      </NavLink>
+      <button onClick={Logout}>LOGOUT</button>
     </div>
   );
 }
--- FrontEnd/src/pages/home/Home.js
+++ FrontEnd/src/pages/home/Home.js
@@ -3,12 +3,9 @@ import { NavLink } from "react-router-dom";
 export default function Home() {
   return (
     <div>
-      <p>Test</p>
+      <p>Home</p>
       <NavLink to={`/article?id=1`}>
         <p>Article</p>
-      </NavLink>
-      <NavLink to={`/login`}>
-        <p>Login</p>
       </NavLink>
     </div>
   );
